fc8b1fa87feb10910f00ec92841093a20b89d9de
SuHwanYoon <ehtldy@naver.com>
2023-11-22 17:18:48 +0900

    add logging level

--- src/main/java/com/yoon/spring/jspTodoApp/login/LoginController.java
+++ src/main/java/com/yoon/spring/jspTodoApp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.yoon.spring.jspTodoApp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	//login?name=Yoon
 	@RequestMapping("login")
 	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name", name);//Key,Value
+		logger.debug("Requset Param is {}", name);
+		logger.info("info level logger");
+		logger.warn("warn level logger");
 		//jsp/login.jsp
 		return "login";
 	}
