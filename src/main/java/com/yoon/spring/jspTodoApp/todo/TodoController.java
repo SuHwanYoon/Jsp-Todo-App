456689a412dc4766a1cf72e9e7c3b520a0464f73
SuHwanYoon <ehtldy@naver.com>
2023-12-04 17:25:54 +0900

    add new todo

--- src/main/java/com/yoon/spring/jspTodoApp/todo/TodoController.java
+++ src/main/java/com/yoon/spring/jspTodoApp/todo/TodoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -29,4 +30,19 @@ public class TodoController {
 		model.addAttribute("todos", todos);
 		return "listTodos";
 	}
+	
+	@RequestMapping(value = "add-todo", method = RequestMethod.GET)
+	public String showNewTodoPage() {
+
+		return "todo";
+	}
+
+	@RequestMapping(value = "add-todo", method = RequestMethod.POST)
+	public String addNewTodo() {
+
+		//return to URL not JSP name and operate listAllTodos Controller Method
+		return "redirect:list-todos";
+	}	
+	
+	
 }
